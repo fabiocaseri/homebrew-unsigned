bb80d5e5560456cb26c834f71a9a2ab826fa17bb
Fabio Caseri <fabio.caseri@gmail.com>
2026-08-24 22:14:54 +0200

    Preserve upstream Homebrew disable status

--- Casks/exifcleaner.rb
+++ Casks/exifcleaner.rb
@@ -16,6 +16,9 @@ cask "exifcleaner" do
     strategy :github_latest
   end
 
+  # Upstream Homebrew status:
+  # disable! date: "2026-09-01", because: :fails_gatekeeper_check
+
   depends_on macos: :monterey
 
   app "ExifCleaner.app"
